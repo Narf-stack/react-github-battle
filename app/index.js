7f32cadfb700b49b2d2146e99f1065ebb76e6305
Frantz Crozilhac <fran.stacksta@gmail.com>
2020-12-12 23:34:30 +0100

    language navbar

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css' // syntax works because of the style-loader for the webpack rules
+import Popular from './components/Popular'
+
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Helllo World!
+      <div className='container'>
+        <Popular />
       </div>
     )
   }
